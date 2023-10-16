b79bd4910e013eeb2afa176863f9663a418670c7
Tyler Jang <tyler@trunk.io>
2023-10-16 16:54:33 -0700

    lint

--- tests/bazel_stub.sh
+++ tests/bazel_stub.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 if [[ $1 == "test" ]]; then
-	echo "Using bazel testing stub for $@"
-	targets_file=$(echo $3 | awk -F "=" '{print $2}')
+	echo "Using bazel testing stub for $*"
+	targets_file=$(echo "$3" | awk -F "=" '{print $2}')
 	diff "${targets_file}" "${TEST_TARGETS_EXPECTED_FILE}"
 	num_targets=$(wc -l "${targets_file}")
 
