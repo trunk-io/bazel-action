35d3efcce873d18bff75f0b0a88d11afc556b0c3
Tyler Jang <tyler@trunk.io>
2023-10-16 17:34:25 -0700

    fix compute workflow

--- tests/setup_bazel_diff.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-_java=$(bazel info java-home)/bin/java
-curl --retry 5 -Lo bazel-diff.jar https://github.com/Tinder/bazel-diff/releases/latest/download/bazel-diff_deploy.jar
-"${_java}" -jar bazel-diff.jar -V
-echo "bazel_diff_cmd=${_java} -jar ./tests/simple_bazel_workspace/bazel-diff.jar" >>"${GITHUB_OUTPUT}"
