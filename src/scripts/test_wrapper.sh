ac211841d61f85c8abbf39d1dc7dfa48bc33c24f
Tyler Jang <tyler@trunk.io>
2023-10-24 13:41:07 -0700

    remove more unused

--- src/scripts/test_wrapper.sh
+++ src/scripts/test_wrapper.sh
@@ -81,7 +81,6 @@ touch "${GITHUB_OUTPUT}"
 
 # Use merge base sha instead of merge head sha to calculate the correct diff for testing.
 MERGE_INSTANCE_BRANCH_HEAD_SHA=$(awk -F "=" '$1=="merge_base_sha" {print $2}' "${GITHUB_OUTPUT}")
-PR_BRANCH=$(awk -F "=" '$1=="pr_branch" {print $2}' "${GITHUB_OUTPUT}")
 PR_BRANCH_HEAD_SHA=$(awk -F "=" '$1=="pr_branch_head_sha" {print $2}' "${GITHUB_OUTPUT}")
 WORKSPACE_PATH=$(awk -F "=" '$1=="workspace_path" {print $2}' "${GITHUB_OUTPUT}")
 BAZEL_DIFF_CMD=$(awk -F "=" '$1=="bazel_diff_cmd" {print $2}' "${GITHUB_OUTPUT}")
